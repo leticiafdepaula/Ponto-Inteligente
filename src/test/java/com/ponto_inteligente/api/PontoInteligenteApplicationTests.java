06768960b6163aa538ea902037a6c8eca86f0b32
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-10-25 13:24:40 -0300

    primeiro commit

---
+++ src/test/java/com/ponto_inteligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponto_inteligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
