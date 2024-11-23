91f580d490164366f2f9250d2ffe7f54e2ffc6a8
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-11-22 22:52:04 -0300

    feat: criando os repositories JPA

--- src/main/java/com/ponto_inteligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/ponto_inteligente/api/repositories/LancamentoRepository.java
@@ -1,5 +1,22 @@
 package com.ponto_inteligente.api.repositories;
 
+import com.ponto_inteligente.api.entities.Lancamento;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Transactional(readOnly = true)
+@NamedQueries ({
+      @NamedQuery (name= "LancamentoRepository.findByFuncionarioId",
+        query= "SELECT lanc FROM lancamento lanc WHERE lanc.funcionario.id = :funcionarioid")
+})
 public interface LancamentoRepository {
 
+    List<Lancamento> findByFuncionarioId(@Param ("funcionarioId")long funcionarioId);
+    Page<Lancamento> findByFuncionarioId(@Param ("funcionarioId")long funcionarioId, Pageable pageable); //para paginar os resultados
 }
